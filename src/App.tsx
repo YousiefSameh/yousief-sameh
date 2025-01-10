cede0c4bee51120a378dc7adf077a05d8ee39afa
Yousief Sameh <yousief.sameh@outlook.com>
2025-01-10 04:21:55 +0200

    Get Started

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline">يا مرحب يا مرحب</div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#2DC653",
+        "hover-color": "#25A244",
+        "border-color": "#383838",
+        "background-contain-color": "#383838",
+        "background-cards-color": "#202021",
+        "background-color": "#1E1E1E",
+      },
+      boxShadow: {
+        "card-shadow": "0px 0px 4px 2px rgb(17, 17, 17, 0.25)",
+      }
+    },
+  },
+  plugins: [],
+}
