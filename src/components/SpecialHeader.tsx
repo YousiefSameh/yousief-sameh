691336c5af79e244d40caa5fd8195326b9016d6d
Yousief Sameh <yousief.sameh@outlook.com>
2025-01-16 01:10:18 +0200

    Make Some Enhance

--- src/components/SpecialHeader.tsx
+++ src/components/SpecialHeader.tsx
@@ -1,15 +1,17 @@
+import React from 'react';
+
 interface SpecialHeaderProps {
-	title: string;
-	id: string;
+  title: string;
+  id: string;
 }
 
-const SpecialHeader = ({ title, id }: SpecialHeaderProps) => {
-	return (
-		<div className="special-header md:mt-0 mt-16" id={id}>
-			<h1 className="text-black dark:text-white font-bold text-4xl">{title}</h1>
-			<span className="line" />
-		</div>
-	);
-};
+const SpecialHeader = React.memo(({ title, id }: SpecialHeaderProps) => {
+  return (
+    <div className="special-header md:mt-0 mt-16" id={id}>
+      <h1 className="text-black dark:text-white font-bold md:text-4xl text-3xl">{title}</h1>
+      <span className="line" />
+    </div>
+  );
+});
 
 export default SpecialHeader;
