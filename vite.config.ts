d4930026c155b85849fabd6ee8935b17224f2879
Yousief Sameh <yousief.sameh@outlook.com>
2025-01-16 15:05:13 +0200

    Edit On Configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-    }),
-  ],
-});
+  plugins: [react()],
+})
