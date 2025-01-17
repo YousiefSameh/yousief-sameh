c44c0f2eb6f726320c58f840df7436dd7dd21fe7
Yousief Sameh <yousief.sameh@outlook.com>
2025-01-17 16:13:51 +0200

    Fix Some Bugs

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,14 +7,14 @@ const About = () => {
 			>
 				من أنا؟
 			</h2>
-			<p className="sm:text-lg text-sm text-black dark:text-white opacity-80 leading-loose">
+			<p className="sm:text-lg text-sm text-black dark:text-white/90 leading-loose">
 				أنا يوسف سامح، مطور واجهات أمامية متخصص في بناء تطبيقات ويب متطورة
 				باستخدام مكتبة React. شغفي يكمن في تحويل الأفكار إلى واجهات مستخدم سلسة
 				وجذابة، تركز على تقديم تجربة استثنائية للمستخدمين. أمتلك خبرة في تصميم
 				وتطوير مواقع متجاوبة وأنيقة، مع الحرص على تحقيق الأداء الأمثل والقابلية
 				للتطوير.
 			</p>
-			<p className="sm:text-lg text-sm text-black dark:text-white opacity-80 leading-loose mt-4">
+			<p className="sm:text-lg text-sm text-black dark:text-white/90 leading-loose mt-4">
 				أتابع باستمرار أحدث التقنيات والاتجاهات في مجال تطوير الويب، مما يمكنني
 				من تقديم حلول مبتكرة وعملية تلبي احتياجات العملاء. أستمتع بالعمل على
 				تحديات جديدة والتعاون مع فرق متعددة التخصصات لإنجاز مشاريع تحقق تأثيرًا
