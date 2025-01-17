14b43233044b151792db1baf8b3df027cc1ec1ce
Yousief Sameh <yousief.sameh@outlook.com>
2025-01-17 17:10:59 +0200

    Fix Some Bugs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteCompression from 'vite-plugin-compression';
 import { imagetools } from 'vite-imagetools';
-import tsconfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
